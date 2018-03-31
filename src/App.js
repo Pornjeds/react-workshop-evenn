99c8d4f5e8fccf77170db24aa27f1f1e13a1431a
Pornjed Sakgitjarung <pornjed.sak@learn.co.th>
2018-03-31 10:58:56 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section className="section">
+        <section className="container">
+          <h1>Evenn Registration Form</h1>
+          <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Text input" />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Username</label>
+            <div class="control has-icons-left has-icons-right">
+              <input class="input is-success" type="text" placeholder="Text input" value="bulma" />
+              <span class="icon is-small is-left">
+                <i class="fas fa-user" />
+              </span>
+              <span class="icon is-small is-right">
+                <i class="fas fa-check" />
+              </span>
+            </div>
+            <p class="help is-success">This username is available</p>
+          </div>
+
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control has-icons-left has-icons-right">
+              <input
+                class="input is-danger"
+                type="email"
+                placeholder="Email input"
+                value="hello@"
+              />
+              <span class="icon is-small is-left">
+                <i class="fas fa-envelope" />
+              </span>
+              <span class="icon is-small is-right">
+                <i class="fas fa-exclamation-triangle" />
+              </span>
+            </div>
+            <p class="help is-danger">This email is invalid</p>
+          </div>
+
+          <div class="field">
+            <label class="label">Ticket Type</label>
+            <div class="control">
+              <div class="select">
+                <select>
+                  <option>Standard</option>
+                  <option>Premium</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Add food?</label>
+            <div class="control">
+              <label class="radio">
+                <input type="radio" name="question" />
+                Yes +50THB
+              </label>
+              <label class="radio">
+                <input type="radio" name="question" />
+                No
+              </label>
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <label class="checkbox">
+                <input type="checkbox" />
+                I agree to the <a href="#">terms and conditions</a>
+              </label>
+            </div>
+          </div>
+
+          <div class="field is-grouped">
+            <div class="control">
+              <button class="button is-link">Submit</button>
+            </div>
+            <div class="control">
+              <button class="button is-text">Cancel</button>
+            </div>
+          </div>
+        </section>
+      </section>
+    );
+  }
+}
+
+export default App;
